ba9fcf7945679449c4bffb57bd32a77567e269d6
Eivind Fonn <evfonn@gmail.com>
2014-08-13 11:29:31 +0200

    Fixes #8 (panning speed)

--- src/GLWidget.cpp
+++ src/GLWidget.cpp
@@ -449,9 +449,9 @@ void GLWidget::mouseMoveEvent(QMouseEvent *event)
         QVector3D right = -(inv * QVector4D(1,0,0,0)).toVector3D(); right.normalize();
         QVector3D up = (inv * QVector4D(0,1,0,0)).toVector3D(); up.normalize();
 
-        float fac = _zoom * tan(_fov * 3.14159265 / 360.0);
+        float fac = _zoom * tan(_fov * 3.14159265 / 360.0) * _diameter;
 
-        setLookAt(mouseOrigLookAt + (shiftPressed ? 2.0 : 20.0) * fac / height() * (
+        setLookAt(mouseOrigLookAt + (shiftPressed ? 0.2 : 2.0) * fac / height() * (
                       right * (event->pos().x() - mouseOrig.x()) +
                       up    * (event->pos().y() - mouseOrig.y())),
                   true);
